e48184d8f4378fbb0b90418d39fb4f0a4ad20148
AlessandroMessori <alessandro1.messori@mail.polimi.it>
2022-03-30 21:10:51 +0200

    added test hierarchical cut

--- src/pacBayesianDendogramCut.py
+++ src/pacBayesianDendogramCut.py
@@ -1,1 +1,30 @@
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+data = pd.read_csv('./data/Wholesale_customer_data.csv')
+data.head()
+
+from sklearn.preprocessing import normalize
+data_scaled = normalize(data)
+data_scaled = pd.DataFrame(data_scaled, columns=data.columns)
+data_scaled.head()
+
+import scipy.cluster.hierarchy as shc
+plt.figure(figsize=(10, 7))  
+plt.title("Dendrograms")  
+dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
+
+import scipy.cluster.hierarchy as shc
+plt.figure(figsize=(10, 7))  
+plt.title("Dendrograms")  
+dend = shc.dendrogram(shc.linkage(data_scaled, method='ward'))
+
+
+from sklearn.cluster import AgglomerativeClustering
+cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')  
+cluster.fit_predict(data_scaled)
+
+plt.figure(figsize=(10, 7))  
+plt.scatter(data_scaled['Milk'], data_scaled['Grocery'], c=cluster.labels_) 
